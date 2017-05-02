d3e4d42b2beeb9bc204d6ec8268d668debd92df2
Remy Radix (Windows) <remy.radix@fontyou.com>
2017-05-02 17:02:06 +0200

    UI: install button text fixed

---
+++ UI/Converters/BoolToInstallStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace UI.Converters {
+  [ValueConversion(typeof(bool), typeof(string))]
+  class BoolToInstallStringConverter : BaseConverter, IValueConverter {
+    #region methods
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+      bool installed = (bool)value;
+      if (installed) {
+        return "Uninstall";
+      }
+      return "Install";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+      return null;
+    }
+    #endregion
+  }
+}
