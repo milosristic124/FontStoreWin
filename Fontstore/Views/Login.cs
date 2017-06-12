4e9918629eef69a738f58b52d1766ae40e0ff07c
Remy Radix (Windows) <remy.radix@fontyou.com>
2017-06-12 13:45:33 +0200

    UI: start of the new UI using Winform. UI: Login view started. UI: Login header with menu done. UI: Drag & drop to move the window added.

---
+++ Fontstore/Views/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using UI.Utilities;
+
+namespace Fontstore {
+  public partial class Login : Form {
+    #region ctor
+    public Login() {
+      InitializeComponent();
+
+      double top, left;
+      TaskBarLocationProvider.CalculateWindowPositionByTaskbar(Width, Height, out left, out top);
+      Top = (int)Math.Floor(top);
+      Left = (int)Math.Floor(left);
+
+      ViewsUtility.SetDragHandle(this, Header);
+    }
+    #endregion
+
+    #region Menu actions
+    private void quitToolStripMenuItem_Click(object sender, EventArgs e) {
+      Application.Exit();
+    }
+
+    private void visitFontstoreToolStripMenuItem_Click(object sender, EventArgs e) {
+      ViewsUtility.NavigateToUri(new Uri("https://www.fontstore.com"));
+    }
+
+    private void helpToolStripMenuItem_Click(object sender, EventArgs e) {
+      ViewsUtility.NavigateToUri(new Uri("https://www.fontstore.com/help"));
+    }
+
+    private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
+      ViewsUtility.ShowAboutPopup();
+    }
+    #endregion
+  }
+}
